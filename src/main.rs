9787498532bba03378f0a907432059e51495beb6
Aditya <adityasinghboss1234@gmail.com>
2024-03-27 06:38:08 +0530

    feat: connected with postgres with sqlx

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+use std::sync::Arc;
+
 use axum::{response::IntoResponse, routing::get, Json, Router};
+use dotenv::dotenv;
+
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 
 async fn health_checker() -> impl IntoResponse {
     const MESSAGE: &str = "Simple CRUD API with Rust, SQLX, Postgres,and Axum";
@@ -11,8 +16,31 @@ async fn health_checker() -> impl IntoResponse {
     Json(json_response)
 }
 
+pub struct AppState {
+    db: Pool<Postgres>,
+}
+
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+
+    let db_url = std::env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+    let pool = match PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&db_url)
+        .await
+    {
+        Ok(pool) => {
+            println!("🚀 Connected to Postgres");
+            pool
+        }
+        Err(err) => {
+            eprintln!("Failed to connect to Postgres: {:?}", err);
+            std::process::exit(1);
+        }
+    };
+
+    let app_state = Arc::new(AppState { db: pool.clone() });
     let app = Router::new().route("/api/healthcheck", get(health_checker));
 
     println!("🚀 Server started successfully");
